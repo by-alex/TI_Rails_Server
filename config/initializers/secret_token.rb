211a62639b21720a12d269e75a2498f44117147d
by-alex <dv.by.alex@gmail.com>
2012-10-21 05:02:44 -0700

    new rails server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TIServer::Application.config.secret_token = '85c8e7144a72c68cde8916d781effa2980fbc03d381ecb534ed5aee937b32d36cda5bcf2d993d459d9ea63b1fc938487cc3cf30e91a474eac2fa075d8bdeb8f7'
